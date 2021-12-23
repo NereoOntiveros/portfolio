de164a69e3db5e31eab55e72c10a3f3c4c9497df
Nereo Ontiveros <nerontiveros@gmail.com>
2021-12-23 01:14:51 +0000

    add multilingual

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,26 @@ const toggleColors = document.getElementById('toggle-colors');
 
 const rootStyles = document.documentElement.style;//get all the variables of the css file
 
+const flagsElement = document.getElementById("flags");
+
+const textsToChange = document.querySelectorAll("[data-section]");
+
+const changeLanguage = async (language) =>{
+    const requestJson = await fetch(`./languages/${language}.json`);
+    const texts = await requestJson.json();
+
+    for(const textToChange of textsToChange){
+        const section = textToChange.dataset.section;
+        const value = textToChange.dataset.value;
+
+        textToChange.innerHTML = texts[section][value];
+    }
+};
+
+flagsElement.addEventListener('click', (e)=>{
+    changeLanguage(e.target.parentElement.dataset.language);
+});
+
 toggleTheme.addEventListener('click', ()=>{
     document.body.classList.toggle('dark');
     if(toggleIcon.src.includes('moon.svg')){
@@ -19,4 +39,4 @@ toggleTheme.addEventListener('click', ()=>{
 
 toggleColors.addEventListener("click", (e)=>{
     rootStyles.setProperty('--primary-color', e.target.dataset.color);//change the primary color to the data attribute's color
-})
+});
